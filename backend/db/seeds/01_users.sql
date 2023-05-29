83e38f95dd43507e7cc404e0ba61930f7e25835a
thahira97 <thahirasheerin3@gmail.com>
2023-05-29 18:46:24 -0400

    Make connection to postgres

---
+++ backend/db/seeds/01_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password)
+VALUES ('Alice', 'alice@gmail.com', 'lollipop');
+
+INSERT INTO users (name, email, password)
+VALUES ('Harry', 'harry@gmail.com', 'icecream');
+
+INSERT INTO users (name, email, password)
+VALUES ('Potter', 'pot@gmail.com', 'candy');
