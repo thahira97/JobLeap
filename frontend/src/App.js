5c9c4fdc7c335c5386ec033e687e86f6e2455fa2
J8051 <nathaniel.jessica@yahoo.ca>
2023-05-31 18:45:05 -0400

    portfolio page skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import {
 import Home from "./components/Home";
 import JobSearch from "./components/JobSearch";
 import "./App.css";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element ={<Profile/>} exact />
         <Route path="/jobs" element={<JobSearch/>} exact />
       </Routes>
     </Router>
