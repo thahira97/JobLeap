251134abfc389e457a26ab8bc8fc8a0619cb8c0b
thahira97 <thahirasheerin3@gmail.com>
2023-06-01 19:49:52 -0400

    Add icons in card

--- frontend/src/components/JobItem.js
+++ frontend/src/components/JobItem.js
@@ -7,6 +7,13 @@ import Button from "./Button";
 
 
 const JobItem = (props) => {
+
+
+  const iconStyles = {
+   color: '#bfc8d9'
+  };
+
+
   return <li className="job-items">
    <Card className="job-item__content">
     <div className="job-item__image">
@@ -15,9 +22,9 @@ const JobItem = (props) => {
     <div className="job-item__info">
       <p className="company-name">{props.name}</p>
       <p>{props.title}</p>
-      <p>{props.location}</p>
+      <p><i className="fa-solid fa-location-dot"></i> {props.location}</p>
       <p>{props.salary}</p>
-      <p>Date Posted: {props.posteddate}</p>
+      <p><i className="fa-solid fa-calendar-days"style={iconStyles}></i> {props.posteddate}</p>
       <p className="company-summary">{props.summary}</p>
       <Button 
           title={props.title}
