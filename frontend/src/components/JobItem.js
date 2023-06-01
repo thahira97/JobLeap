3fdc8c90695395779e4ee917acc55df283f7dc57
thahira97 <thahirasheerin3@gmail.com>
2023-06-01 11:57:50 -0400

    Show props in JobItem

--- frontend/src/components/JobItem.js
+++ frontend/src/components/JobItem.js
@@ -2,11 +2,21 @@ import React from "react";
 
 import "./JobItem.css";
 import Card from "./Card";
+import Avatar from "./Avatar"
 
 const JobItem = (props) => {
   return <li className="job-items">
    <Card className="job-item__content">
-    
+    <div className="job-item__image">
+     <Avatar image={props.image} alt={props.title} />
+    </div>
+    <div className="job-item__info">
+      <h2>{props.title}</h2>
+      <h2>{props.location}</h2>
+      <h2>{props.description}</h2>
+      <h2>{props.salary}</h2>
+      <h2>{props.posteddate}</h2>
+    </div>
    </Card>
   </li>;
 };
--- frontend/src/components/JobLists.js
+++ frontend/src/components/JobLists.js
@@ -2,12 +2,37 @@ import React from "react";
 
 import "./JobLists.css";
 import JobItem from "./JobItem";
+import Card from "./Card";
 
 const JobLists = (props) => {
-  return <div className="job-lists">
-    <p className="job-header">Recommended jobs</p>
-    <JobItem />
-  </div>;
+  if (props.jobs.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h1>No jobs Found</h1>
+        </Card>
+      </div>
+    );
+  }
+  return (
+    <div className="job-lists">
+      <p className="job-header">Recommended jobs</p>
+      {props.jobs.map((job) => {
+        return (
+          <JobItem
+            key={job.id}
+            id={job.id}
+            image={job.compImg}
+            title={job.title}
+            location={job.location}
+            description={job.description}
+            salary={job.salary}
+            posteddate={job.datePosted}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default JobLists;
