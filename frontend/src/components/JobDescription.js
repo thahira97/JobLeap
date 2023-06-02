6d6179d1f1da07f3cfadb11ad70ef7eff658489c
thahira97 <thahirasheerin3@gmail.com>
2023-06-02 11:27:55 -0400

    Fix css before merging

--- frontend/src/components/JobDescription.js
+++ frontend/src/components/JobDescription.js
@@ -1,15 +1,36 @@
 import React from "react";
 
 import "./JobDescription.css";
+import Card from "./Card";
 
 const JobDescription = (props) => {
+
+const moneyIconStyle={
+  color: "#5a8774",
+};
+const computerIconStyle={
+  color: "#7ba4d1",
+};
+const cardStyling={
+backgroundColor: "#FFDEB9",
+border: "1px",
+boxShadow: "none"
+}
+
 return <div className="job-description">
-  <p>{props.name}</p>
-  <p>{props.title}</p>
-  <p>{props.salary}</p>
-  <p>{props.description}</p>
-  <p>{props.schedule}</p>
-  <p>{props.flextime}</p>
+  <div className="description-header">
+      <Card style={cardStyling}><p>{props.name}</p>
+  <span>{props.title}</span></Card>
+  </div>
+
+  <aside><b>About the job</b></aside>
+  <p>{props.description} </p>
+  <ul className="detailed-list">
+  <li>Schedule: <i class="fa-solid fa-suitcase"></i> {props.schedule}</li>  
+  <li>FlexTime: <i className="fa-solid fa-computer"style={computerIconStyle}></i> {props.flextime}</li>
+  <li>Salary: <i className="fa-solid fa-money-check-dollar"style={moneyIconStyle}></i> {props.salary}</li>
+  </ul>
+  
   <div className="main-buttons">
     <div className="apply-button">
     <button class="btn btn-primary" type="submit">Apply using old resume</button>
--- frontend/src/components/JobSearch.js
+++ frontend/src/components/JobSearch.js
@@ -45,7 +45,46 @@ const dummyData = [
     location: "Calgary",
     summary:"Full stack-developers have a range of responsibilities from shaping and implementing digital products.",
     description:
-      "Full stack-developers have a range of responsibilities from shaping and implementing digital products to ensuring that we stay on the leading edge of technology.Our developers take an important role in developing the solution's complete architecture and delivery, and are responsible for creating and maintaining the integration and deployment pipeline (DevOps)",
+      `WHAT YOU’LL GET OUT OF IT:
+
+      $17-$22 Per Hour + Additional Bonus’
+      Bi-weekly payroll company events
+      Work with your friends and get to work outside in the spring/summer months
+      Grow your management and leadership skills
+      REQUIREMENTS:
+      
+      Positive Attitude (We hire for attitude, train for skill)
+      No experience needed, TRAINING IS PROVIDED for both safety and painting
+      Full time commitment: 40-50 hrs/ Per Week
+      Detail Oriented and Results Driven
+      Ability to work at heights (not afraid of using ladders)
+      Reliable Transportation
+      Please submit your resume if interested in applying.
+      
+      Job Type: Full-time
+      
+      Salary: $16.50-$24.00 per hour
+      
+      Benefits:
+      
+      Automobile allowance
+      Casual dress
+      Company events
+      On-site parking
+      Profit sharing
+      Flexible Language Requirement:
+      
+      French not required
+      Schedule:
+      
+      8 hour shift
+      Monday to Friday
+      Supplemental pay types:
+      
+      Bonus pay
+      Overtime pay
+      Retention bonus
+      Tips`,
     salary: "$80,000",
     schedule: "Part-time",
     flexTime: "Hybrid",
@@ -64,6 +103,51 @@ const dummyData = [
     salary: "$80,000",
     schedule: "Internship",
     flexTime: "Remote",
+    datePosted: "19/3/2023",
+  },
+  {
+    id: 1,
+    companyName: "Walmart",
+    compImg:
+      "https://media.licdn.com/dms/image/C560BAQHs5FN9hz6seQ/company-logo_100_100/0/1654192382941?e=1693440000&v=beta&t=DokNoPXWD9qbhQHkugkQwB_AA5UmwMSnxBcMkVaarSA",
+    title: "Full-Stack Developer",
+    location: "Calgary",
+    summary:"Full stack-developers have a range of responsibilities from shaping and implementing digital products.",
+    description:
+      "Full stack-developers have a range of responsibilities from shaping and implementing digital products to ensuring that we stay on the leading edge of technology.Our developers take an important role in developing the solution's complete architecture and delivery, and are responsible for creating and maintaining the integration and deployment pipeline (DevOps)",
+    salary: "$80,000",
+    schedule: "Full-time",
+    flexTime: "On-site",
+    datePosted: "19/3/2023",
+  },
+  {
+    id: 1,
+    companyName: "Walmart",
+    compImg:
+      "https://media.licdn.com/dms/image/C560BAQHs5FN9hz6seQ/company-logo_100_100/0/1654192382941?e=1693440000&v=beta&t=DokNoPXWD9qbhQHkugkQwB_AA5UmwMSnxBcMkVaarSA",
+    title: "Full-Stack Developer",
+    location: "Calgary",
+    summary:"Full stack-developers have a range of responsibilities from shaping and implementing digital products.",
+    description:
+      "Full stack-developers have a range of responsibilities from shaping and implementing digital products to ensuring that we stay on the leading edge of technology.Our developers take an important role in developing the solution's complete architecture and delivery, and are responsible for creating and maintaining the integration and deployment pipeline (DevOps)",
+    salary: "$80,000",
+    schedule: "Full-time",
+    flexTime: "On-site",
+    datePosted: "19/3/2023",
+  },
+  {
+    id: 1,
+    companyName: "Walmart",
+    compImg:
+      "https://media.licdn.com/dms/image/C560BAQHs5FN9hz6seQ/company-logo_100_100/0/1654192382941?e=1693440000&v=beta&t=DokNoPXWD9qbhQHkugkQwB_AA5UmwMSnxBcMkVaarSA",
+    title: "Full-Stack Developer",
+    location: "Calgary",
+    summary:"Full stack-developers have a range of responsibilities from shaping and implementing digital products.",
+    description:
+      "Full stack-developers have a range of responsibilities from shaping and implementing digital products to ensuring that we stay on the leading edge of technology.Our developers take an important role in developing the solution's complete architecture and delivery, and are responsible for creating and maintaining the integration and deployment pipeline (DevOps)",
+    salary: "$80,000",
+    schedule: "Full-time",
+    flexTime: "On-site",
     datePosted: "19/3/2023",
   },
 ];
