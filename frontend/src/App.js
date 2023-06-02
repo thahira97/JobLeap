7cf2aaddfe4507c563ca534deb22cf6ef0b509d1
J8051 <nathaniel.jessica@yahoo.ca>
2023-06-02 11:53:09 -0400

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import Home from "./components/Home";
 import JobSearch from "./components/JobSearch";
 import "./App.css";
 import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+import Login from "./components/Login"
+import ForgotPassword from "./components/ForgotPassword"
 
 function App() {
   return (
@@ -18,6 +21,18 @@ function App() {
         </Route>
         <Route path="/jobs" exact>
           <JobSearch />
+        </Route>
+        <Route path = "/user" exact>
+          <Profile/>
+        </Route>
+        <Route path = "/signup" exact>
+          <Signup/>
+        </Route>
+        <Route path = "/login" exact>
+          <Login/>
+        </Route>
+        <Route path = "/forgot-password" exact>
+          <ForgotPassword/>
         </Route>
         <Redirect to="/" />
       </Switch>
