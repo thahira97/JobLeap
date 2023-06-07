33c29e18a2f2f7dce216cabd5a0464e92f752781
thahira97 <thahirasheerin3@gmail.com>
2023-06-07 17:44:06 -0400

    Set up chatGPT config

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const express = require("express");
 const cors = require('cors');
+const fetch = require('cross-fetch');
+const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-
 const jobsRoutes = require("./routes/jobs-routes");
 
 const app = express();
@@ -13,8 +14,34 @@ const app = express();
 ///////////////
 
 app.use(cors());
+app.use(express.json())
 app.use(bodyParser.json())
 app.use('/api/jobs',jobsRoutes);
+
+
+app.post('/completions', async(req, res)=> {
+  const options = {
+    method: 'POST',
+    headers: {
+      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({
+      model : "gpt-3.5-turbo",
+      messages: [{ role: 'user', content: 'how are you?'}],
+      max_tokens: 100,
+    })
+  }
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+    const data = await response.json()
+    res.send(data)
+  }
+  catch(error) {
+    console.log(error)
+  }
+})
+
 
 
 app.listen(PORT, () => {
