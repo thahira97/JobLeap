48f2c173666b8674aef9cf8458535ead8e85ab2b
thahira97 <thahirasheerin3@gmail.com>
2023-06-07 10:40:28 -0400

    Set state for other options

--- frontend/src/components/FilterSearch.js
+++ frontend/src/components/FilterSearch.js
@@ -3,15 +3,29 @@ import { useState } from "react";
 import "./FilterSearch.css";
 
 const FilterSearch = (props) => {
-  const [locationState, setLocationState] = useState("");
-  const [dateState, setDateState] = useState("");
-  const [scheduleState, setScheduleState] = useState("");
-  const [flexTimeState, setFlexTimeState] = useState("");
-  const [salaryState, setSalaryState] = useState("");
+  // const [locationState, setLocationState] = useState("");
+  // const [dateState, setDateState] = useState("");
+  // const [scheduleState, setScheduleState] = useState("");
+  // const [flexTimeState, setFlexTimeState] = useState("");
+  // const [salaryState, setSalaryState] = useState("");
+  const [selectedState, setSelectedState] = useState({
+    location: "",
+    datePosted: "",
+    schedule: "",
+    flexTime: "",
+    salary: ""
+  })
 
-  const selectedOption = (event) => {
-    setLocationState(event.target.value)
+  const selectedOptionHandler = (event) => {
+   const name = event.target.name;
+   const value = event.target.value;
+
+   setSelectedState((prev)=>{
+    return {...prev, [name]: value}
+   })
   }
+ 
+
   return (
     <div className="search-filter">
       <div className="container text-center">
@@ -21,10 +35,11 @@ const FilterSearch = (props) => {
               <select
                 className="btn btn-outline--light"
                 aria-label="Default select example"
-                value={locationState}
-                onChange={selectedOption}
+                name="location"
+                value={selectedState.location}
+                onChange={selectedOptionHandler}
               >
-                <option >Location</option>
+                <option value="">Location</option>
                 <option value="Toronto">Toronto</option>
                 <option value="Hamilton">Hamilton</option>
                 <option value="Windsor">Windsor</option>
@@ -36,8 +51,11 @@ const FilterSearch = (props) => {
               <select
                 className="btn btn-outline--light"
                 aria-label="Default select example"
+                name="datePosted"
+                value={selectedState.datePosted}
+                onChange={selectedOptionHandler}
               >
-                <option >Date-Posted</option>
+                <option value="" >Date-Posted</option>
                 <option value="Past 24 hrs">Past 24 hrs</option>
                 <option value="Past-week">Past-week</option>
                 <option value="Past-month">Past-month</option>
@@ -47,6 +65,9 @@ const FilterSearch = (props) => {
               <select
                 className="btn btn-outline--light"
                 aria-label="Default select example"
+                name="schedule"
+                value={selectedState.schedule}
+                onChange={selectedOptionHandler}
               >
                 <option>Schedule</option>
                 <option value="Full-time">Full-time</option>
@@ -58,6 +79,9 @@ const FilterSearch = (props) => {
               <select
                 className="btn btn-outline--light"
                 aria-label="Default select example"
+                name="flexTime"
+                value={selectedState.flexTime}
+                onChange={selectedOptionHandler}
               >
                 <option>On-site/remote</option>
                 <option value="On-site<">On-site</option>
@@ -69,6 +93,9 @@ const FilterSearch = (props) => {
               <select
                 className="btn btn-outline--light"
                 aria-label="Default select example"
+                name="salary"
+                value={selectedState.salary}
+                onChange={selectedOptionHandler}
               >
                 <option>Salary</option>
                 <option value="$40,000+">$40,000+</option>
