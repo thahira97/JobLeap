9f621c222a13f1b30750cb3525d08a260c4b93b6
thahira97 <thahirasheerin3@gmail.com>
2023-06-08 19:23:26 -0400

    Add more datas to DB

--- backend/db/seeds/05_job_listings.sql
+++ backend/db/seeds/05_job_listings.sql
@@ -5,9 +5,39 @@ INSERT INTO job_listings (company_name,company_image, job_title,job_summary,job_
 VALUES ('Simons','https://media.licdn.com/dms/image/C560BAQGUm3JLSio8uw/company-logo_100_100/0/1592510036576?e=1694044800&v=beta&t=DmTZoEABMIfaut3Nb0FrtiDzUjQK7pkb5PMsUn6_YWQ','Sales Associate','Every day, we’re empowered to work together to help our customers make fast, confident planning decisions' ,'Provide high-quality customer service to our clientele. Perform various tasks within the assigned department, such as merchandizing products. Enjoy fashion and interacting with people', '$65,000','Hamilton','Part-Time','On-site','3 days ago' );
 
 INSERT INTO job_listings (company_name,company_image, job_title,job_summary,job_description, salary, location ,schedule,flex_time, date_posted)
-VALUES ('United Nations','https://media.licdn.com/dms/image/C4E0BAQGd_5g0zDG5VA/company-logo_100_100/0/1679890370488?e=1694044800&v=beta&t=9zuuKvxlrYzmvy3IrbG1cw8FFqvzuQQjlEpMHp9uBNU' ,'Administrative Assistant','The Human Resources Manager reports to the Senior Human Resources Business Partner and supports 4 facilities', 'Processing of visas for UN official travels. Provides guidance to UN sponsored delegates travelling to SCBD meetings on current visa procedures and responds to related queries. Provides guidance to staff members in response to queries related to visa requirements. Liaises and coordinates extensively with all Embassies/High Commissions regarding regulations/requirements for visas. Verifies correctness of visa application forms issued to staff members for visa applications. ', '$60,000','Windsor','Internship','Hybrid', '1 day ago');
+VALUES ('Carters','https://media.licdn.com/dms/image/C560BAQGEtL-xqfQmWQ/company-logo_100_100/0/1658431443670?e=1694044800&v=beta&t=tWlnfX80MP9c5Z_sLin3FJ1skgWsasEvbeGl7mZI11U' ,'Administrative Assistant','The Human Resources Manager reports to the Senior Human Resources Business Partner and supports 4 facilities', 'Processing of visas for UN official travels. Provides guidance to UN sponsored delegates travelling to SCBD meetings on current visa procedures and responds to related queries. Provides guidance to staff members in response to queries related to visa requirements. Liaises and coordinates extensively with all Embassies/High Commissions regarding regulations/requirements for visas. Verifies correctness of visa application forms issued to staff members for visa applications. ', '$60,000','Windsor','Internship','Hybrid', '1 day ago');
 
 INSERT INTO job_listings (company_name,company_image, job_title,job_summary,job_description, salary, location ,schedule,flex_time, date_posted)
 VALUES ('Nations','https://media.licdn.com/dms/image/C4E0BAQGd_5g0zDG5VA/company-logo_100_100/0/1679890370488?e=1694044800&v=beta&t=9zuuKvxlrYzmvy3IrbG1cw8FFqvzuQQjlEpMHp9uBNU' ,'Administrative Assistant','The Human Resources Manager reports to the Senior Human Resources Business Partner and supports 4 facilities', 'Processing of visas for UN official travels. Provides guidance to UN sponsored delegates travelling to SCBD meetings on current visa procedures and responds to related queries. Provides guidance to staff members in response to queries related to visa requirements. Liaises and coordinates extensively with all Embassies/High Commissions regarding regulations/requirements for visas. Verifies correctness of visa application forms issued to staff members for visa applications. ', '$60,000','Oakville','Internship','Hybrid','1 month ago');
 
+INSERT INTO job_listings (company_name, company_image, job_title, job_summary, job_description, salary, location, schedule,flex_time, date_posted)
+VALUES ('Nextdoor', 'https://media.licdn.com/dms/image/C560BAQEz4eJn3nBsLA/company-logo_100_100/0/1654723721267?e=1694044800&v=beta&t=Y82Nkn-J8reui0iZBrrwfpc_4TeYHf1_fHXKiqp7HFo','Full-Stack developer', 'Nextdoor is where you connect to the neighborhoods that matter to you so you can belong. Our purpose is to cultivate a kinder world where everyone has a neighborhood they can rely on.', 'What will Bring to The Team
+5+ Years of experience in a Mobile Development role
+Expertise in Swift or Objective-C and Xcode IDE including the build system
+Robust knowledge of iOS development ecosystem as a whole including system libraries
+Experience with building complex, high-quality iOS applications, and strong knowledge of iOS app design principles, patterns, testing, and best practices
+Strong collaboration and communication skills, both verbal and written
+Strong desire to learn about new technologies and systems
+Ability to be flexible and adaptable in a dynamic startup environment
+Bonus Points
+Experience writing automated tests for iOS applications
+Experience with accessibility features on iOS.', '$60,000', 'Toronto','Internship','Hybrid', '1 day ago');
 
+
+INSERT INTO job_listings (company_name,company_image, job_title,job_summary,job_description, salary, location ,schedule,flex_time, date_posted)
+VALUES ('Bridge', 'https://media.licdn.com/dms/image/C4D0BAQGsGTXL-_Tkgw/company-logo_100_100/0/1647980376943?e=1694044800&v=beta&t=Ru1BtFgZUjDGw4PKD4ecl62KuZhVV5y3jJ5TMGjvAjk', 'Full-Stack developer', 'Clearbridge Mobile, an Amdocs company, is an award-winning mobile app developer that designs and develops best-in-class mobile solutions for clients including PayPal, Tim Hortons, Rogers, Bell, TD Bank, and USA Today.', 'As a Front-end developer, you will be involved in the entire software development lifecycle, from conception to completion. Your responsibilities will include:
+Building efficient, reusable well-designed and high-quality front-end systems using React.js and Angular 9+
+Translating designs and wireframes into high-quality code
+Designing highly responsive web-based user interfaces
+Building reusable components and front-end libraries for future
+Debugging & optimizing code to enable maximum performance across devices and browsers', '$65,000', 'Windsor','Part-Time' ,'On-site', '1 week ago');
+
+INSERT INTO job_listings (company_name,company_image, job_title,job_summary,job_description, salary, location ,schedule,flex_time, date_posted)
+VALUES ('CGI', 'https://media.licdn.com/dms/image/C4E0BAQErzXWSFkn9tQ/company-logo_100_100/0/1663664110409?e=1694044800&v=beta&t=w-nPM5ZTEJhAfDdCv-AQTi9rFVDLpouWYvl7tHakwPM', 'Full-Stack developer', 'As a Full Stack Developer, you will be using the latest tools and technologies available to deliver state-of-the-art software and web applications. You will have an opportunity to drive the choice of technologies used as we transform our applications and experiment with the best new tools', 'Design, develop and/or re-engineer complex application components, and integrate software packages, programs and reusable objects residing on multiple platforms.Be responsible for maintenance and improvement of applications lifecycle
+Perform application development based on Agile methodologies & participate in requirements / user story analysis, design and execution.Conduct appropriate unit and module testing; detect, report, investigate and fix defects in the application (production support)', '$70,000', 'Toronto','Full-Time', 'Remote', '1 day ago');
+
+INSERT INTO job_listings (company_name,company_image, job_title,job_summary,job_description, salary, location ,schedule,flex_time, date_posted)
+VALUES ('Dropbox', 'https://media.licdn.com/dms/image/C560BAQHjnNsmL5L2NA/company-logo_100_100/0/1654114493079?e=1694044800&v=beta&t=Dc_F3gUbW3XsAsRNfv0Sxt1ISBeARLo89ax6qkSyQuk', 'Full-Stack developer', 'Dropbox is a special place where we are all seeking to fulfill our mission to design a more enlightened way of working. We are looking for innovative talent to join us on our journey.', 'Own projects end to end, starting with data discovery, design, implementation, and deployment
+Develop customer-centric prototypes for new product explorations
+Proactively communicate your learnings and progress to your team and the leadership
+Own relationships with other engineering teams and collaborate with other functions within Dropbox', '$60,000', 'Windsor', 'Internship', 'Remote', '3 days ago');
