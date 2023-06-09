0729a3da34d8007b98d89705b60b5784f1968a70
thahira97 <thahirasheerin3@gmail.com>
2023-06-08 22:28:32 -0400

    Setup config for chatGPT

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,28 @@ app.use('/api/resumes', resumesRoutes);
 ////////////////////////////////////////////////////////////
 // Listener
 ////////////////////////////////////////////////////////////
+app.post('/completions', async(req, res)=> {
+  const options = {
+    method: 'POST',
+    headers: {
+      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({
+      model : "gpt-3.5-turbo",
+      messages: [{ role: 'user', content: 'how are you?'}],
+      max_tokens: 100,
+    })
+  }
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+    const data = await response.json()
+    res.send(data)
+  }
+  catch(error) {
+    console.log(error)
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
